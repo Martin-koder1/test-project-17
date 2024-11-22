b8d95279d157a115eb3581ab853ac3a765a493c9
Martin-koder1 <martinmartinkoder@gmail.com>
2024-11-22 13:47:54 +0100

    stuff and fixes

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const submitBtn = document.querySelector('#submit-btn');
 const nameInput = document.querySelector('#name');
 const surnameInput = document.querySelector('#surname');
 
+//hobby Eleementer
+const fiskeElement = document.querySelector('#fiske')
+const gamingElement = document.querySelector('#gaming')
+const kodingElement = document.querySelector('#koding')
+
 // land element
 const countryInput = document.querySelector('#country');
 
@@ -14,6 +19,19 @@ function submit(event) {
     const name = nameInput.value;
     const surname = surnameInput.value;
     const country = countryInput.value;
-}
 
 submitBtn.addEventListener('click', submit);
+
+const hobbyArray [];
+
+if (fiskeElement.checked === true) {
+    hobbyArray.push(fiskeElement.value)
+}
+if {gamingElement.checked === true} {
+    hobbyArray.push(gamingElement.value)
+}
+if (kodingElement.checked === true) {
+    hobbyArray.push(kodingElement.value)
+}
+
+}
